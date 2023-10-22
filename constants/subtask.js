e60ba2d47aaf1e0c8756bac57714cae7ca1f1439
lpcuong2106 <lpcuong2106@gmail.com>
2023-10-22 22:16:19 +0700

    create sub task

---
+++ constants/subtask.js
@@ -0,0 +1,7 @@
+export const SUB_TASKS = {
+    SELF_CHECK: 'Self check',
+    WRITE_DOCUMENT: 'Write Document',
+    VOLLEYBALL_MEETING: 'Volleyball meeting',
+    WRITE_UT: 'Write UT',
+    WRITE_IT: 'Write IT/SVT',
+}
--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-import chalk from 'chalk'
 import inquirer from 'inquirer'
-import fs from 'fs'
-import * as envfile from 'envfile'
-import { writeEnvToFile } from './writeEnv.js'
-import { configDotenv } from 'dotenv'
+import {writeEnvToFile} from './writeEnv.js'
+import {configDotenv} from 'dotenv'
 import axios from 'axios'
+import {createSpinner} from "nanospinner";
+import {SUB_TASKS} from "./constants/subtask.js";
+import chalk from "chalk";
 
 configDotenv();
 
 let username = process.env.USERNAME_JIRA;
 let password = process.env.PASSWORD_JIRA;
 let JIRA_URL = process.env.JIRA_URL;
+let allStories = []
 
 async function inputAccount() {
     if (!JIRA_URL) {
@@ -23,7 +24,7 @@ async function inputAccount() {
             }
         })
         JIRA_URL = jiraInput.jiraUrl
-        writeEnvToFile([{ key: 'JIRA_URL', value: jiraUrl }])
+        writeEnvToFile([{key: 'JIRA_URL', value: JIRA_URL}])
     }
 
     if (!username) {
@@ -36,7 +37,7 @@ async function inputAccount() {
             }
         })
         username = answersInput.username
-        writeEnvToFile([{ key: 'USERNAME_JIRA', value: username }])
+        writeEnvToFile([{key: 'USERNAME_JIRA', value: username}])
     }
 
 
@@ -50,21 +51,81 @@ async function inputAccount() {
             }
         })
         password = passwordInput.password
-        writeEnvToFile([{ key: 'PASSWORD_JIRA', value: password }])
+        writeEnvToFile([{key: 'PASSWORD_JIRA', value: password}])
     }
 }
 
 
 async function getJiraSprints() {
-   const res = await axios.get(`${JIRA_URL}/rest/agile/1.0/sprint/14667/issue?maxResults=100`, {
-    auth: {
-        username: username,
-        password: password
+    const spinner = createSpinner('Checking stories of the active spring...').start();
+    const {data: res} = await axios.get(`${JIRA_URL}/rest/agile/1.0/sprint/14667/issue?maxResults=100`, {
+        auth: {
+            username: username,
+            password: password
+        }
+    })
+
+    const stories = res.issues.filter(issue => issue.fields.issuetype.name === 'Story');
+    allStories = stories.map(story => story.key);
+
+    spinner.success();
+    const {storiesChoosen} = await inquirer.prompt({
+        name: 'storiesChoosen',
+        type: 'checkbox',
+        loop: false,
+        message: 'What stories would you like to create the subtasks?',
+        choices: ['All', ...stories.map(stories => stories.key)]
+    })
+    return storiesChoosen;
+
+}
+
+async function createSubtaskForStories(stories, allStories) {
+    if (stories.includes('All')) {
+        createSubTask(allStories)
+    } else {
+        createSubTask(stories.filter(story => story !== 'All'))
     }
-   })
-   console.log(res.data)
 }
 
 
+function createSubTask(stories) {
+    stories.forEach(async story => {
+        let createTaskSpin = createSpinner();
+        let subTaskCreated = []
+        for (let key in SUB_TASKS) {
+            try {
+                const body = {
+                    "fields": {
+                        "description": "",
+                        "issuetype": {
+                            "id": "10101"
+                        },
+                        "parent": {
+                            "key": story
+                        },
+                        "project": {
+                            "id": "22702"
+                        },
+                        "summary": SUB_TASKS[key]
+                    }
+                }
+                await axios.post(`${JIRA_URL}/rest/api/2/issue`, body, {
+                    auth: {
+                        username,
+                        password
+                    }
+                })
+                subTaskCreated.push(SUB_TASKS[key])
+            } catch (e) {
+                console.log(chalk.red(`ERROR: ${e.message} \n`))
+            }
+        }
+        console.log(chalk.green(`${story}: sub-task ${subTaskCreated.join(', ')} is created\n`))
+        createTaskSpin.success({text: `Create successfully sub-task for story ${story}`})
+    })
+}
+
 await inputAccount();
-await getJiraSprints();
+const storiesChoosen = await getJiraSprints();
+await createSubtaskForStories(storiesChoosen, allStories)
